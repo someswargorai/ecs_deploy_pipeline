a18188d6e24df5cb35c2f9654dc14f71a2735bbb
someswargorai <goraisomeswar@gmail.com>
2025-07-12 19:07:31 +0530

    feat: pushing to dev branch

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const cors=require("cors");
+const app=express();
+
+app.use(cors({
+    origin:"*"
+}));
+app.get("/",(req,res)=>{
+    res.status(200).send("ECS demo using fargate with working fine like pro 🔥");
+})
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.listen(3001, '0.0.0.0', () => {
+  console.log('Server running on port 3001');
+});
